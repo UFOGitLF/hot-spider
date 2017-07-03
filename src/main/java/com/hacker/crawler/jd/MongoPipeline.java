2e1e21521031854712378f85d2d07d5a793c7da3
UFOsubject <you@example.com>
2017-07-03 11:31:53 +0800

    fix

--- src/main/java/com/hacker/crawler/jd/MongoPipeline.java
+++ src/main/java/com/hacker/crawler/jd/MongoPipeline.java
@@ -10,22 +10,32 @@ import org.bson.Document;
 
 import java.util.List;
 
+/**
+ * 对抽取出来的ProductList进行入库操作，使用mongodb
+ * 
+ * @Title MongoPipeline.java
+ * @Description
+ * @author Succy
+ * @date 2016年11月17日 下午2:46:53
+ * @version 1.0
+ * @Company www.succy.cn
+ */
 @PipelineName("mongoPipeline")
 public class MongoPipeline implements Pipeline<ProductList> {
 
-    @Override
-    public void process(ProductList productList) {
-        MongoCollection<Document> collection = MongoDBUtil.instance.getCollection("test","jd_spider");
-        HttpRequest req = productList.getRequest();
-        // 从productList里边获取url，目的是为了从之前存进数据库中找到对应url的小类目
-        String url = req.getUrl();
-        // 把类目名对应的商品详情的列表获取，例如，手机对应到的页面的60条记录
-        List<ProductDetail> details = productList.getDetails();
-        // 转成json字符串
-        String jsonString = JSON.toJSONString(details);
-        // 通过url找到数组里边对应url的类目，然后添加一个字段叫做details，并且把details的值
-        // 给添加进去
-        collection.updateOne(new Document("categories.url", url),
-                Document.parse("{\"$set\":{\"categories.$.details\":" + jsonString + "}}"));
-    }
+	@Override
+	public void process(ProductList productList) {
+		MongoCollection<Document> collection = MongoDBUtil.instance.getCollection("test","jd_spider");
+		HttpRequest req = productList.getRequest();
+		// 从productList里边获取url，目的是为了从之前存进数据库中找到对应url的小类目
+		String url = req.getUrl();
+		// 把类目名对应的商品详情的列表获取，例如，手机对应到的页面的60条记录
+		List<ProductDetail> details = productList.getDetails();
+		// 转成json字符串
+		String jsonString = JSON.toJSONString(details);
+		// 通过url找到数组里边对应url的类目，然后添加一个字段叫做details，并且把details的值
+		// 给添加进去
+		collection.updateOne(new Document("categories.url", url),
+				Document.parse("{\"$set\":{\"categories.$.details\":" + jsonString + "}}"));
+	}
 }
--- src/main/java/com/hacker/crawler/jd/ProductList.java
+++ src/main/java/com/hacker/crawler/jd/ProductList.java
@@ -5,24 +5,48 @@ import com.geccocrawler.gecco.annotation.HtmlField;
 import com.geccocrawler.gecco.annotation.Request;
 import com.geccocrawler.gecco.request.HttpRequest;
 import com.geccocrawler.gecco.spider.HtmlBean;
-import lombok.Data;
 
 import java.util.List;
 
-@Data
-@Gecco(matchUrl = "https://list.jd.com/list.html?cat={cat}", pipelines = { "consolePipeline", "filePipeline" ,"mongoPipeline"})
+/**
+ * 商品列表实体类，对应的是京东的类目连接到的页面的每页60条记录
+ * @Title ProductList.java
+ * @Description
+ * @author Succy
+ * @date 2016年11月19日 下午1:58:37
+ * @version 1.0
+ * @Company www.succy.cn
+ */
+@Gecco(matchUrl = "https://list.jd.com/list.html?cat={cat}", pipelines = { "consolePipeline",
+		"filePipeline" ,"mongoPipeline"})
 public class ProductList implements HtmlBean {
 
-    private static final long serialVersionUID = -6580138290566056728L;
+	private static final long serialVersionUID = -6580138290566056728L;
 
-    /**
-     * 获取请求对象，从该对象中可以获取抓取的是哪个url
-     */
-     @Request
-     private HttpRequest request;
+	/**
+	 * 获取请求对象，从该对象中可以获取抓取的是哪个url
+	 */
+	 @Request
+	 private HttpRequest request;
 
-    // #plist > ul > li.gl-item > div.j-sku-item
-    @HtmlField(cssPath = "#plist > ul > li.gl-item")
-    private List<ProductDetail> details;
+	// #plist > ul > li.gl-item > div.j-sku-item
+	@HtmlField(cssPath = "#plist > ul > li.gl-item")
+	private List<ProductDetail> details;
+
+	 public HttpRequest getRequest() {
+	 return request;
+	 }
+	
+	 public void setRequest(HttpRequest request) {
+	 this.request = request;
+	 }
+
+	public List<ProductDetail> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<ProductDetail> details) {
+		this.details = details;
+	}
 
 }
